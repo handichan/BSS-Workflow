199da9c6cfe84053200e28411931a417f7d58b9a
mcpigman <94017858+mcpigman@users.noreply.github.com>
2025-01-17 15:29:10 -0800

    Download from all scenarios at the same time

--- sql/data_downloads/county_ann_eu.sql
+++ sql/data_downloads/county_ann_eu.sql
@@ -1,13 +1,34 @@
 -- county annual end use totals
 
 SELECT "in.county",turnover,"in.state","year",sector,end_use,sum(county_ann_kwh) as county_ann_kwh 
-FROM county_annual_breakthrough_amy -- variable?
+FROM long_county_annual_breakthrough_amy -- variable?
 WHERE county_ann_kwh >= 0
 GROUP BY "in.county",turnover,"in.state","year",sector,end_use
 
 UNION all
 SELECT "in.county",turnover,"in.state","year",sector,end_use,sum(county_ann_kwh) as county_ann_kwh 
-FROM county_annual_ineff_amy -- variable?
+FROM long_county_annual_high_amy -- variable?
+WHERE turnover!='baseline' -- don't need the baseline from all scenarios
+AND county_ann_kwh >= 0
+GROUP BY "in.county",turnover,"in.state","year",sector,end_use
+
+UNION all
+SELECT "in.county",turnover,"in.state","year",sector,end_use,sum(county_ann_kwh) as county_ann_kwh 
+FROM long_county_annual_ineff_amy -- variable?
+WHERE turnover!='baseline' -- don't need the baseline from all scenarios
+AND county_ann_kwh >= 0
+GROUP BY "in.county",turnover,"in.state","year",sector,end_use
+
+UNION all
+SELECT "in.county",turnover,"in.state","year",sector,end_use,sum(county_ann_kwh) as county_ann_kwh 
+FROM long_county_annual_mid_amy -- variable?
+WHERE turnover!='baseline' -- don't need the baseline from all scenarios
+AND county_ann_kwh >= 0
+GROUP BY "in.county",turnover,"in.state","year",sector,end_use
+
+UNION all
+SELECT "in.county",turnover,"in.state","year",sector,end_use,sum(county_ann_kwh) as county_ann_kwh 
+FROM long_county_annual_stated_amy -- variable?
 WHERE turnover!='baseline' -- don't need the baseline from all scenarios
 AND county_ann_kwh >= 0
 GROUP BY "in.county",turnover,"in.state","year",sector,end_use
--- sql/data_downloads/county_monthly_maxes.sql
+++ sql/data_downloads/county_monthly_maxes.sql
@@ -1,7 +1,19 @@
 -- find top hour per month in each county; sum of res and com
 
 WITH sub as(SELECT "in.county",timestamp_hour,extract(month from timestamp_hour) as "month",turnover,"in.state","year",county_hourly_kwh 
-FROM county_hourly_breakthrough_amy -- variable?
+FROM long_county_hourly_breakthrough_amy -- variable?
+UNION all
+SELECT "in.county",timestamp_hour,extract(month from timestamp_hour) as "month",turnover,"in.state","year",county_hourly_kwh 
+FROM long_county_hourly_high_amy WHERE turnover != 'baseline'
+UNION all
+SELECT "in.county",timestamp_hour,extract(month from timestamp_hour) as "month",turnover,"in.state","year",county_hourly_kwh 
+FROM long_county_hourly_ineff_amy WHERE turnover != 'baseline'
+UNION all
+SELECT "in.county",timestamp_hour,extract(month from timestamp_hour) as "month",turnover,"in.state","year",county_hourly_kwh 
+FROM long_county_hourly_mid_amy WHERE turnover != 'baseline'
+UNION all
+SELECT "in.county",timestamp_hour,extract(month from timestamp_hour) as "month",turnover,"in.state","year",county_hourly_kwh 
+FROM long_county_hourly_stated_amy WHERE turnover != 'baseline'
 ),
 
 county_hourly_totals as(
