dfdf6a129d431f34afc3e89d817d83dcd6655677
mcpigman <94017858+mcpigman@users.noreply.github.com>
2025-01-17 15:18:40 -0800

    Download from all scenarios at the same time

--- sql/data_downloads/county_100_hrs.sql
+++ sql/data_downloads/county_100_hrs.sql
@@ -2,12 +2,30 @@
 
 with county_totals as(
 SELECT "in.county",timestamp_hour,turnover,"in.state","year",sum(county_hourly_kwh) as county_total_hourly_kwh 
-FROM county_hourly_breakthrough_amy 
+FROM long_county_hourly_breakthrough_amy 
 GROUP BY "in.county",timestamp_hour,turnover,"in.state","year"
 
 UNION all
 SELECT "in.county",timestamp_hour,turnover,"in.state","year",sum(county_hourly_kwh) as county_total_hourly_kwh 
-FROM county_hourly_ineff_amy 
+FROM long_county_hourly_high_amy 
+WHERE turnover!='baseline' -- don't need the baseline from both scenarios
+GROUP BY "in.county",timestamp_hour,turnover,"in.state","year"
+
+UNION all
+SELECT "in.county",timestamp_hour,turnover,"in.state","year",sum(county_hourly_kwh) as county_total_hourly_kwh 
+FROM long_county_hourly_ineff_amy 
+WHERE turnover!='baseline' -- don't need the baseline from both scenarios
+GROUP BY "in.county",timestamp_hour,turnover,"in.state","year"
+
+UNION all
+SELECT "in.county",timestamp_hour,turnover,"in.state","year",sum(county_hourly_kwh) as county_total_hourly_kwh 
+FROM long_county_hourly_mid_amy 
+WHERE turnover!='baseline' -- don't need the baseline from both scenarios
+GROUP BY "in.county",timestamp_hour,turnover,"in.state","year"
+
+UNION all
+SELECT "in.county",timestamp_hour,turnover,"in.state","year",sum(county_hourly_kwh) as county_total_hourly_kwh 
+FROM long_county_hourly_stated_amy 
 WHERE turnover!='baseline' -- don't need the baseline from both scenarios
 GROUP BY "in.county",timestamp_hour,turnover,"in.state","year"
 )
