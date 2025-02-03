d5ebfb9ebeaa2f0d8b7c69801fce6375ff811c59
mcpigman <94017858+mcpigman@users.noreply.github.com>
2025-02-03 10:49:39 -0800

    Update long_to_wide_ann.sql to pull from all scout_annual_state_* tables at once

--- sql/data_downloads/long_to_wide_ann.sql
+++ sql/data_downloads/long_to_wide_ann.sql
@@ -1,8 +1,31 @@
-WITH agg as(SELECT "year", reg, turnover, fuel, sum(state_ann_kwh) as state_ann_kwh
-    -- convert to variable
-FROM scout_annual_state_breakthrough
-WHERE turnover != 'baseline'
-GROUP BY "year", reg, turnover, fuel),
+WITH agg as(
+    SELECT "year", reg, turnover, fuel, sum(state_ann_kwh) as state_ann_kwh
+    FROM scout_annual_state_stated
+    GROUP BY "year", reg, turnover, fuel
+    
+    UNION ALL
+    SELECT "year", reg, turnover, fuel, sum(state_ann_kwh) as state_ann_kwh
+    FROM scout_annual_state_mid
+    WHERE turnover != 'baseline'
+    GROUP BY "year", reg, turnover, fuel    
+
+    UNION ALL
+    SELECT "year", reg, turnover, fuel, sum(state_ann_kwh) as state_ann_kwh
+    FROM scout_annual_state_high
+    WHERE turnover != 'baseline'
+    GROUP BY "year", reg, turnover, fuel    
+    
+    UNION ALL
+    SELECT "year", reg, turnover, fuel, sum(state_ann_kwh) as state_ann_kwh
+    FROM scout_annual_state_breakthrough
+    WHERE turnover != 'baseline'
+    GROUP BY "year", reg, turnover, fuel    
+    
+    UNION ALL
+    SELECT "year", reg, turnover, fuel, sum(state_ann_kwh) as state_ann_kwh
+    FROM scout_annual_state_ineff
+    WHERE turnover != 'baseline'
+    GROUP BY "year", reg, turnover, fuel),
 
 formatted_cols as(
 SELECT turnover as scenario, "year", reg as state, fuel, state_ann_kwh
