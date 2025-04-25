e99245750c157eb3ee4eeb949b8959b816d16144
mcpigman <94017858+mcpigman@users.noreply.github.com>
2025-04-24 17:38:12 -0700

    Update table names, upgrades to 2024.2

--- sql/com/com_hourly_shares_refrig.sql
+++ sql/com/com_hourly_shares_refrig.sql
@@ -12,8 +12,8 @@ ts_not_agg AS (
 		WHEN extract(YEAR FROM DATE_TRUNC('hour', from_unixtime(ts."timestamp" / 1000000000)) + INTERVAL '1' HOUR) = 2019 THEN DATE_TRUNC('hour', from_unixtime(ts."timestamp" / 1000000000)) - INTERVAL '1' YEAR + INTERVAL '1' HOUR
 		ELSE DATE_TRUNC('hour', from_unixtime(ts."timestamp" / 1000000000)) + INTERVAL '1' HOUR END as timestamp_hour,
 		ts."out.electricity.refrigeration.energy_consumption" * meta.weight as refrigeration
-	FROM "comstock_amy2018_release_2024.1_by_state" as ts
-		RIGHT JOIN "comstock_amy2018_release_2024.1_metadata" as meta 
+	FROM "comstock_amy2018_release_2024.2_by_state" as ts
+		RIGHT JOIN "comstock_amy2018_release_2024.2_parquet" as meta 
 		ON ts.bldg_id = meta.bldg_id
 		AND ts.upgrade = cast(meta.upgrade as varchar)
 	WHERE ts.upgrade = '0'
