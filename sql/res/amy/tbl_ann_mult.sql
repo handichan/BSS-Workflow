f55b19dea41617375f395976ce623908d11fb323
mcpigman <94017858+mcpigman@users.noreply.github.com>
2025-06-05 17:40:07 -0700

    Add in.weather_file_city to table initialization

--- sql/res/amy/tbl_ann_mult.sql
+++ sql/res/amy/tbl_ann_mult.sql
@@ -1,6 +1,7 @@
 -- to hold annual geographic disaggregation multipliers calculated from BuildStock
 CREATE EXTERNAL TABLE res_annual_disaggregation_multipliers_VERSIONID(
     `in.county` string,
+    `in.weather_file_city` string,
     group_ann string,
     multiplier_annual double,
     sector string,
