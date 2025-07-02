7f40bebf1e9e58475826a72a74d395e101816281
mcpigman <94017858+mcpigman@users.noreply.github.com>
2025-07-01 17:53:06 -0700

    Res multipliers don't have counties!

--- sql/run_check/test_multipliers_hourly_res_eu.sql
+++ sql/run_check/test_multipliers_hourly_res_eu.sql
@@ -2,12 +2,6 @@ with n_eu as(
 SELECT "in.state", "in.weather_file_city",COUNT(DISTINCT(end_use)) as n_uses 
 FROM res_hourly_disaggregation_multipliers_20250616_amy 
 WHERE multiplier_hourly=multiplier_hourly 
-GROUP BY "in.state","in.weather_file_city"),
-insuff as(
+GROUP BY "in.state","in.weather_file_city")
 SELECT * from n_eu 
-WHERE n_uses!=8)
-SELECT insuff.*, geo_map.county_name, geo_map.population
-FROM insuff
-LEFT JOIN geo_map
-ON insuff."in.county"=geo_map."stock.county"
-ORDER BY geo_map.population;
+WHERE n_uses!=8;
