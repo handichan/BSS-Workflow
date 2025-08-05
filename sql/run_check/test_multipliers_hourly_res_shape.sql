08a3f6d9e9a321d5d5a2ad2a753319074c1eb0c4
mcpigman <94017858+mcpigman@users.noreply.github.com>
2025-08-05 10:14:25 -0700

    superseded by test_multipliers_hourly_shape.sql

--- sql/run_check/test_multipliers_hourly_res_shape.sql
+++
@@ -1,8 +0,0 @@
-with n_weath as(
-SELECT shape_ts,COUNT(DISTINCT("in.weather_file_city","in.state")) as n_weather 
-FROM res_hourly_disaggregation_multipliers_20250616_amy 
-WHERE multiplier_hourly=multiplier_hourly 
-GROUP BY shape_ts)
-SELECT * from n_weath 
-WHERE n_weather!=1215 
-ORDER BY shape_ts;
