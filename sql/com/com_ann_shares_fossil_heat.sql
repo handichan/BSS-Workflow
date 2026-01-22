6980607f41e5691a5ce64886239cdc9ed0d2ed79
Yujie Xu <yujiex@5c-e9-1e-83-4f-ff.dhcp.lbnl.us>
2026-01-21 17:48:46 -0800

    remove reference to char.version sql script

--- sql/com/com_ann_shares_fossil_heat.sql
+++ sql/com/com_ann_shares_fossil_heat.sql
@@ -6,7 +6,6 @@ WITH meta_filtered AS (
 	SELECT meta."in.nhgis_county_gisjoin",
 	    meta."in.state",
 		chars.group_ann,
-        chars."version",
 		sum(meta."calc.weighted.natural_gas.heating.energy_consumption..tbtu" + meta."calc.weighted.other_fuel.heating.energy_consumption..tbtu" + meta."calc.weighted.electricity.heat_recovery.energy_consumption..tbtu") as heating
 	FROM "{meta_com}" as meta
 		RIGHT JOIN (SELECT * FROM com_ann_hvac WHERE group_ann in ('com_hvac_ann_12','com_hvac_ann_24')) as chars ON meta."in.heating_fuel" = chars."in.heating_fuel"
@@ -16,16 +15,14 @@ WITH meta_filtered AS (
 	GROUP BY 
 		meta."in.nhgis_county_gisjoin",
 		meta."in.state",
-		chars.group_ann,
-        chars."version"
+		chars.group_ann
 ),
 geo_shares AS (
     SELECT "in.nhgis_county_gisjoin",
     "in.state",
-    "version",
     group_ann,
     heating,
-    heating / sum(heating) OVER (PARTITION BY "in.state", group_ann, "version") as heating_mult
+    heating / sum(heating) OVER (PARTITION BY "in.state", group_ann) as heating_mult
 FROM meta_filtered
 )
     SELECT 
