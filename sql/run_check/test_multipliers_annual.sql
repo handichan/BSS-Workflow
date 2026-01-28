ac87f1f143c030f86be2e6dc9f3cf48f41d0ac82
Yujie Xu <yujiex@5c-e9-1e-83-4f-ff.dhcp.lbnl.us>
2026-01-28 11:34:31 -0800

    parameterize table names in test_multipliers sql

--- sql/run_check/test_multipliers_annual.sql
+++ sql/run_check/test_multipliers_annual.sql
@@ -1,3 +1,3 @@
 SELECT end_use,COUNT(DISTINCT("in.county")) as n_counties 
-FROM {sector}_annual_disaggregation_multipliers_{version}
+FROM {sector}_annual_{suffix}
 WHERE multiplier_annual=multiplier_annual GROUP BY end_use;
--- sql/run_check/test_multipliers_hourly_com.sql
+++ sql/run_check/test_multipliers_hourly_com.sql
@@ -1,3 +1,3 @@
 SELECT end_use,COUNT(DISTINCT("in.county")) as n_counties 
-FROM {sector}_hourly_disaggregation_multipliers_{version}
+FROM {sector}_hourly_{suffix}
 WHERE multiplier_hourly=multiplier_hourly GROUP BY end_use;
