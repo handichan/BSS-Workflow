ed5a9aa57529238f322eda30443d5878d557299c
Yujie Xu <yujiex@5c-e9-1e-83-4f-ff.dhcp.lbnl.us>
2026-03-24 11:14:17 -0700

    add in.weather_file_longitude in test_multipliers_hourly_res

--- sql/run_check/test_multipliers_hourly_res.sql
+++ sql/run_check/test_multipliers_hourly_res.sql
@@ -1,3 +1,3 @@
-SELECT end_use,COUNT(DISTINCT("in.weather_file_city","in.state")) as n_weatherfile_city 
+SELECT end_use,COUNT(DISTINCT("in.weather_file_city", "in.weather_file_longitude", "in.state")) as n_weatherfile_city 
 FROM {sector}_hourly_{suffix}
 WHERE multiplier_hourly=multiplier_hourly GROUP BY end_use;
