d71f5af856a07035afc46d786746cabe9943d69e
Yujie Xu <yujiex@5c-e9-1e-83-4f-ff.dhcp.lbnl.us>
2026-05-12 10:43:52 -0700

    fix rebasing issue

--- sql/com/com_ann_shares_wh.sql
+++ sql/com/com_ann_shares_wh.sql
@@ -14,11 +14,9 @@ WITH meta_filtered AS (
 		meta."in.nhgis_county_gisjoin",
 		meta."in.state",
 		chars.group_ann
-)
-SELECT  "in.nhgis_county_gisjoin" as "in.county",
-    group_ann,
-    wh / sum(wh) OVER (PARTITION BY "in.state", group_ann) as multiplier_annual,
-    'com' AS sector,
+),
+geo_totals AS (
+    SELECT "in.nhgis_county_gisjoin",
     "in.state",
     group_ann,
     wh_elec,
